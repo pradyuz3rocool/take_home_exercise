c104908494be70e316873de4f4c3f76ba38fbb9a
Altay Adademir <altayadademir@gmail.com>
2017-10-30 10:37:20 +0000

    Quote model updated.

--- src/main/java/com/shutl/model/Quote.java
+++ src/main/java/com/shutl/model/Quote.java
@@ -1,21 +1,48 @@
 package com.shutl.model;
 
-public class Quote {
-    String pickupPostcode;
-    String deliveryPostcode;
-    Long price;
+import com.shutl.enums.Vehicle;
+import org.json.simple.JSONArray;
 
-    public Quote() {}
+public class Quote {
+
+    private String pickupPostcode;
+    private String deliveryPostcode;
+    /**
+     * BigDecimal type can be used for price var instead of Long (since BigDecimal is more precise), as suggested by
+     * official Oracle Java docs - https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
+     * A different type may be used to improve space complexity.
+     */
+    private Long price;
+    private Vehicle vehicle;
+    private JSONArray priceList;
+
+    public Quote() {
+    }
 
     public Quote(String pickupPostcode, String deliveryPostcode) {
-        this.pickupPostcode = pickupPostcode;
-        this.deliveryPostcode = deliveryPostcode;
+        this.setPickupPostcode(pickupPostcode);
+        this.setDeliveryPostcode(deliveryPostcode);
     }
 
     public Quote(String pickupPostcode, String deliveryPostcode, Long price) {
-        this.pickupPostcode = pickupPostcode;
-        this.deliveryPostcode = deliveryPostcode;
-        this.price = price;
+        this.setPickupPostcode(pickupPostcode);
+        this.setDeliveryPostcode(deliveryPostcode);
+        this.setPrice(price);
+    }
+
+    public Quote(String pickupPostcode, String deliveryPostcode, Long price, String vehicle) {
+        this.setPickupPostcode(pickupPostcode);
+        this.setDeliveryPostcode(deliveryPostcode);
+        this.setPrice(price);
+        this.setVehicle(Vehicle.valueOf(vehicle));
+    }
+
+    public Quote(String pickupPostcode, String deliveryPostcode, Long price, String vehicle, JSONArray priceList) {
+        this.setPickupPostcode(pickupPostcode);
+        this.setDeliveryPostcode(deliveryPostcode);
+        this.setPrice(price);
+        this.setVehicle(Vehicle.valueOf(vehicle));
+        this.setPriceList(priceList);
     }
 
     public String getPickupPostcode() {
@@ -23,7 +50,8 @@ public class Quote {
     }
 
     public void setPickupPostcode(String pickupPostcode) {
-        this.pickupPostcode = pickupPostcode;
+        // Get rid of the spaces
+        this.pickupPostcode = pickupPostcode.replaceAll("\\s+", "");
     }
 
     public String getDeliveryPostcode() {
@@ -31,7 +59,8 @@ public class Quote {
     }
 
     public void setDeliveryPostcode(String deliveryPostcode) {
-        this.deliveryPostcode = deliveryPostcode;
+        // Get rid of the spaces
+        this.deliveryPostcode = deliveryPostcode.replaceAll("\\s+", "");
     }
 
     public Long getPrice() {
@@ -40,5 +69,21 @@ public class Quote {
 
     public void setPrice(Long price) {
         this.price = price;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public JSONArray getPrice_list() {
+        return priceList;
+    }
+
+    public void setPriceList(JSONArray priceList) {
+        this.priceList = priceList;
     }
 }
